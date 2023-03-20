d782500fc3cfd49b95751cd6b825f63605513b79
jedwilshire <jedwilshire@gmail.com>
2023-03-20 14:42:51 -0500

    changed lesson 5 to use pygame.SCALED and vsync

--- Lesson 05 - Advanced Sprite Movement/main.py
+++ Lesson 05 - Advanced Sprite Movement/main.py
@@ -1,10 +1,13 @@
 import pygame
 from settings import *
 from writer import *
-# Global Variables
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# SCALED is a flag to make display resolution depend on
+# diplay resolution so small graphics show big on 4k displays
+# Scaled is required to use vsynce, vertical sync that is
+screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.SCALED, vsync=1)
+
 screen.fill(CYAN)
-pygame.display.flip()
 myGroup = pygame.sprite.Group()
 mySprite = pygame.sprite.Sprite(myGroup)
 dt = 0
@@ -18,11 +21,11 @@ def createSprite():
     mySprite.rect = mySprite.image.get_rect()
     mySprite.rect.center = (WIDTH // 2, HEIGHT // 2)
     mySprite.pos = [mySprite.rect.x, mySprite.rect.y]
-    mySprite.dx = 60
-    mySprite.dy = 60
+    mySprite.dx = 100 # move 100 pixels right or left per second
+    mySprite.dy = 100 # move 100 pixels right or left per second
     
 def update():
-    mySprite.pos[0] += mySprite.dx * dt/1000
+    mySprite.pos[0] += mySprite.dx * dt/1000 
     mySprite.pos[1] += mySprite.dy * dt/1000
     mySprite.rect.topleft = mySprite.pos
     if mySprite.rect.right >= WIDTH:
@@ -41,8 +44,8 @@ def draw():
     screen.fill(CYAN)
     # if not in a sprite group use .blit
     # screen.blit(mySprite.image, mySprite.rect)
-    writer.setText(str(dt))
-    writer.writeText(200, 200)
+#     writer.setText(str(dt))
+#     writer.writeText(200, 200)
     myGroup.draw(screen)
     pygame.display.flip()
 
@@ -56,11 +59,10 @@ def onKeyDown(key):
     pass
 
 def mainloop():
-    global dt
+    global dt # allows us to change global value dt
     running = True
     clock = pygame.time.Clock()
     while running:
-        dt = clock.tick_busy_loop(60)
         update()
         draw()
         for event in pygame.event.get():
@@ -73,7 +75,7 @@ def mainloop():
                 onMouseDown(event.pos[0], event.pos[1])
             elif event.type == pygame.KEYDOWN:
                 onKeyDown(event.key)
-        
+        dt = clock.tick_busy_loop(60) # dt is milliseconds since last update
 createSprite()
 pygame.init()
 mainloop()
